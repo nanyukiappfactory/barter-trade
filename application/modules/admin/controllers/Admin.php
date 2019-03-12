be90c9a245885210d67365c4258f087beefbcd1e
wagachari <sarafinagachari@gmail.com>
2019-03-12 15:00:38 +0300

    ..

--- application/modules/admin/controllers/Admin.php
+++ application/modules/admin/controllers/Admin.php
@@ -9,7 +9,7 @@ class Admin extends MX_Controller
          {
             parent::__construct();
             $this->load->model("auth/auth_model");
-            $this->load->library('session');
+            //$this->load->library('session');
             // if(!$this->auth_model->validate_session())
             // {
             //    redirect('admin/login');
