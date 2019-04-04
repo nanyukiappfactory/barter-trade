7bb1b68d99677729a230900087e938e66e7fa420
wagachari <sarafinagachari@gmail.com>
2019-04-04 15:41:31 +0300

    cors handled

--- application/modules/admin/models/Locations_model.php
+++ application/modules/admin/models/Locations_model.php
@@ -5,6 +5,27 @@ class Locations_model extends CI_Model
         public function __construct()
         {
             parent::__construct();
+            if (isset($_SERVER['HTTP_ORIGIN'])) {
+
+                header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+                
+                header('Access-Control-Allow-Credentials: true');
+                
+                header('Access-Control-Max-Age: 86400'); // cache for 1 day
+                
+                 }
+                // Access-Control headers are received during OPTIONS requests
+                if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+                    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+                 }
+                if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+                    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+                
+                 }
+                exit(0);
+                
+                 }
         }
     public function get_locations()
     {
@@ -18,7 +39,7 @@ class Locations_model extends CI_Model
                
                 ->where("nearby_location.nearby_location_status",1);
         $my_locations = $this->db->get()->result();
-         
+
         return $my_locations;
     }
     public function insertCounties($insert_data)
